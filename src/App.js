7e7aedf1d10827545c8bb40590fbc1fc51836cb3
kjhto46 <tony1996kjh@gmail.com>
2023-07-24 22:44:36 +0900

    useEffect 이용해서 리렌더링 확이

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("항상실행");
+  useEffect(() => {
+    console.log("한번만 실행되는 useEffect");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 3) {
+      console.log("검색 중", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
-      <h1>시작해보기</h1>
-      <Button text={"계속하기"} />
+      <input onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>클릭하기</button>
     </div>
   );
 }
