05d061d3e09b3232fcc77ef448e332e9594b8740
kjhto46 <tony1996kjh@gmail.com>
2023-07-26 02:35:53 +0900

    해야할 일 리스트 만들기 추가만되기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  // function byFn() {
-  //   console.log("종료하기 🥹");
-  // }
-  // function hiFn() {
-  //   console.log('생성하기 😀');
-  //   return byFn;
-  // }
-  // useEffect(hiFn, []); // 이런식으로 return을 이용해 useEffect의 보이는 반대도 제어 가능, 단 아래처럼 useEffect안에 다 쓰는 방향을 선호
-  useEffect(() => {
-    console.log("생성하기 😀");
-    return () => {
-      console.log('종료하기 🥹')
-    }
-  }, []);
-  return <h1>안녕하세요?</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => {
+    setToDo(e.target.value);
+    // console.log(toDo);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "숨김" : "보이기"}</button>
+      <h1>해야할 일 ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="해야할일 작성"
+        ></input>
+        <button>메모하기</button>
+      </form>
+      <ul>
+        {toDos.map((i, index) => (
+          <li key={index}>{i}</li>
+        ))}
+      </ul>
     </div>
   );
 }
