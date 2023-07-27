a79fe8707f2379ed99cc8020a85ba7397484af5e
kjhto46 <tony1996kjh@gmail.com>
2023-07-28 01:55:39 +0900

    API를 이용한 영화 정리 사이트

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => {
-      response.json().then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-    });
+    getMovies();
   }, []);
+  console.log(movies);
   return (
     <div>
-      <h1>The coins! ({coins.length})</h1>
-      {loading ? <strong>로딩중..</strong> : null}
-      <ul>
-        {coins.map((coinApi) => (
-          <li key={coinApi.id}>{coinApi.name} ({coinApi.symbol}) : ${coinApi.quotes.USD.price}</li>
-        ))}
-      </ul>
+      {loading ? (
+        <h1>로딩중...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <figure><img src={movie.medium_cover_image} /></figure>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
